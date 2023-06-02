bf616f7c4410c96da28586995a9fb9ad2ede780b
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-06-02 12:59:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,183 +79,222 @@ resource "aws_security_group" "usac-sg" {
     tags = {
      name  =  "usac-security-group"
    }
-}   
+}  
+#######variables.tf
+module "C1B-Workloads-BankApp-UAT-01" {
+  source = "./modules/aft-account-request"
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  version = "2.10.0"
-  providers = {
-    aws.primary   = aws.primary
-    aws.secondary = aws.secondary
+  control_tower_parameters = {
+    AccountEmail              = "aws.bnk.app.uat01@creditone.com"
+    AccountName               = "C1B-Workloads-BankApp-UAT-01"
+    ManagedOrganizationalUnit = "Staging"
+    SSOUserEmail              = "aws.bnk.app.uat01@creditone.com"
+    SSOUserFirstName          = "C1B-Workloads-BankApp-UAT-01"
+    SSOUserLastName           = "Creditone"
   }
-  count = length(var.buckets)
-  bucket = var.buckets[count.index]
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-  versioning = {
-    enabled = true
+
+  account_tags = {
+      "c1b:BusinessUnit"       = "IT"
+      "c1b:CostCenter"         = "0320"
+      "c1b:Department"         = "Cloud Infrastructure"
+      "c1b:Owner"              = "Cloud_Operations_Team@creditone.com"
+      "c1b:ContactDistroEmail" = "Cloud_Operations_Team@creditone.com"
+      "c1b:Environment"        = "UA"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Cloud Infrastructure"
+    change_reason       = "Workloads-BankApp-UAT-01"
+  }
+
+  custom_fields = {
+    group = "Workloads-BankApp-UAT-01"
+  }
+
+  account_customizations_name = "C1B-Workloads-BankApp-UAT-01"
 }
-  force_destroy = true
-server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm ="AES256"
+#######ddb.tf
+    resource "aws_dynamodb_table_item" "account-request" {
+  table_name = var.account-request-table
+  hash_key   = var.account-request-table-hash
+
+  item = jsonencode({
+    id = { S = lookup(var.control_tower_parameters, "AccountEmail") }
+    control_tower_parameters = { M = {
+      AccountEmail              = { S = lookup(var.control_tower_parameters, "AccountEmail") }
+      AccountName               = { S = lookup(var.control_tower_parameters, "AccountName") }
+      ManagedOrganizationalUnit = { S = lookup(var.control_tower_parameters, "ManagedOrganizationalUnit") }
+      SSOUserEmail              = { S = lookup(var.control_tower_parameters, "SSOUserEmail") }
+      SSOUserFirstName          = { S = lookup(var.control_tower_parameters, "SSOUserFirstName") }
+      SSOUserLastName           = { S = lookup(var.control_tower_parameters, "SSOUserLastName") }
       }
     }
-}
-tags = merge(var.tags,{
-    Name = "${var.buckets[count.index]}"
+    change_management_parameters = { M = {
+      change_reason       = { S = lookup(var.change_management_parameters, "change_reason") }
+      change_requested_by = { S = lookup(var.change_management_parameters, "change_requested_by") }
+      }
+    }
+    account_tags                = { S = jsonencode(var.account_tags) }
+    account_customizations_name = { S = var.account_customizations_name }
+    custom_fields               = { S = jsonencode(var.custom_fields) }
   })
 }
-resource "aws_iam_user" "bucket_user" {
-    count = length(var.buckets)
-    name = "${var.buckets[count.index]}"    
+    
+    
+    #######variables.tf
+variable "account-request-table" {
+  type        = string
+  description = "name of account-request-table"
+  default     = "aft-request"
 }
-resource "aws_iam_policy" "bucket_policy" { 
-    count = length(var.buckets)
-    name = "${var.buckets[count.index]}" 
-    policy = jsonencode(
-      {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": "arn:aws-us-gov:s3:::${var.buckets[count.index]}/*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListAllMyBuckets"
-            ],
-            "Resource": "arn:aws-us-gov:s3:::${var.buckets[count.index]}/*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListBucket",
-                "s3:GetBucketLocation"
-            ],
-            "Resource": "arn:aws-us-gov:s3:::${var.buckets[count.index]}"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:DeleteObject",
-                "s3:DeleteObjectVersion"
-            ],
-            "Resource": "arn:aws-us-gov:s3:::${var.buckets[count.index]}/*"
-        }
-    ]
-}
-    )
- }
-  resource "aws_iam_user_policy_attachment" "bucket_policy_attachment" {
-    count = length(var.buckets)
-    user = aws_iam_user.bucket_user[count.index].name
-    policy_arn = aws_iam_policy.bucket_policy[count.index].arn   
-  }
 
-  resource "aws_s3_bucket_policy" "s3_bucket" {
-    count = length(var.buckets)
-    bucket = var.buckets[count.index]
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "Policy1643297892890",
-    "Statement": [
-        {
-            "Sid": "Stmt1643297888916",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS":  "arn:aws-us-gov:iam::${var.id}:user/${aws_iam_user.bucket_user[count.index].name}"
-            },
-            "Action": "s3:*",
-            "Resource": "arn:aws-us-gov:s3:::${var.buckets[count.index]}"
-        }
-    ]
-})
+variable "account-request-table-hash" {
+  type        = string
+  description = "name of account-request-table hash key"
+  default     = "id"
 }
-data "aws_iam_policy_document" "secondary" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}" 
-  statement {
-    actions = [
-      "s3:ReplicateObject",
-      "s3:ReplicateDelete",
-      "s3:ReplicateTags",
-    ]
-    resources = ["${aws_s3_bucket.secondary[count.index].arn}/*"]
+
+variable "control_tower_parameters" {
+  type = object({
+    AccountEmail              = string
+    AccountName               = string
+    ManagedOrganizationalUnit = string
+    SSOUserEmail              = string
+    SSOUserFirstName          = string
+    SSOUserLastName           = string
+  })
+}
+
+variable "change_management_parameters" {
+  type = object({
+    change_requested_by = string
+    change_reason       = string
+  })
+}
+
+variable "account_tags" {
+  type        = map(any)
+  description = "map of account-level tags"
+}
+
+variable "custom_fields" {
+  type        = map(any)
+  description = "map of custom fields defined by the customer"
+  default     = {}
+}
+
+variable "account_customizations_name" {
+  type        = string
+  default     = null
+  description = "The name of the account customizations to apply"
+}
+    
+    
+#######main
+    account id creation
+    locals {
+  environment_prefix = "${var.platform}-${var.platform_half}-${var.environment}"
+}
+
+module "vpc-module" {
+  source  = "app.terraform.io/creditonebank/vpc-module/aws"
+  version = "1.1.6"
+  name    = "${local.environment_prefix}-vpc"
+  cidr    = var.vpc_cidr
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  enable_vpn_gateway = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  transit_gateway_id = "tgw-0473c470df70bb9bf"
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+
+  map_public_ip_on_launch = false
+   private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
-data "aws_iam_policy_document" "s3-assume-role" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}"
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
+    
+    
+    main.tf
+    
+    output "account_id" {
+  value = var.account_id
 }
-resource "aws_s3_bucket_replication_configuration" "primary" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}"
-  depends_on = [aws_s3_bucket_versioning.primary[count.index]]
-  role   = aws_iam_role.replication[count.index].arn
-  bucket = aws_s3_bucket.primary[count.index].bucket
-  rule {
-    id = aws_s3_bucket.secondary[count.index].bucket
-    filter {} 
-    status = "Enabled"
-    delete_marker_replication {
-      status = "Enabled"
-    }
-    destination {
-      bucket = aws_s3_bucket.secondary[count.index].arn
-    }
-  }
+
+output "private_subnets" {
+  value = module.vpc-module.private_subnets
 }
-data "aws_iam_policy_document" "primary" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}"
-  statement {
-    actions = [
-      "s3:GetReplicationConfiguration",
-      "s3:ListBucket",
-    ]
-    resources = ["arn:aws-us-gov:s3:::${var.primary_name}"]
-  }
-  statement {
-    actions = [
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging",
-    ]
-    resources = ["arn:aws-us-gov:s3:::${var.primary_name}/*"]
-  }
+
+output "platform" {
+  value = var.platform
 }
-resource "aws_iam_role" "replication" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}"
-  assume_role_policy = data.aws_iam_policy_document.s3-assume-role[count.index].json
+
+output "platform_half" {
+  value = var.platform_half
 }
-resource "aws_iam_role_policy" "replication-primary" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}-primary"
-  role   = aws_iam_role.replication[count.index].name
-  policy = data.aws_iam_policy_document.primary[count.index].json
+
+output "environment" {
+  value = var.environment
 }
-resource "aws_iam_role_policy" "replication-secondary" {
-  count = length(var.buckets)
-  name = "${var.buckets[count.index]}-secondary"
-  role   = aws_iam_role.replication[count.index].name
-  policy = data.aws_iam_policy_document.secondary[count.index].json
+
+output "vpc_id" {
+  value = module.vpc-module.vpc_id
 }
+
+output "project_name" {
+  value = var.project_name
+}
+
+output "environment_tag" {
+  value = var.environment_tag
+}
+      
+      variable "platform" {}
+variable "platform_half" {}
+variable "environment" {}
+variable "vpc_cidr" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "account_id" {}
+variable "workspace_name" {}
+variable "project_name" {}
+variable "environment_tag" {}
+      
+      #####vpc modules
+      
+      ocals {
+  max_subnet_length = max(
+    length(var.private_subnets),
+    length(var.elasticache_subnets),
+    length(var.database_subnets),
+    length(var.redshift_subnets),
+  )
+  nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = element(
+    concat(
+      aws_vpc_ipv4_cidr_block_association.this.*.vpc_id,
+      aws_vpc.this.*.id,
+      [""],
+    ),
+    0,
+  )
+}
+      
+      
+      ####
+
 
